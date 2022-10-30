09369ec3204ac3af7494e32aeebd3906f07c10ee
Delainee Lenss <lenssd@oregonstate.edu>
2022-10-30 15:04:33 -0500

    Commit Message

---
+++ find_median.py
@@ -0,0 +1,16 @@
+# Author: Delainee Lenss
+# GitHub username: delainee64
+# Date: 10/30/2022
+# Description: Write a function that takes as a parameter a list of numbers. The function
+# should return the statistical median of those numbers, which will require it to sort the list.
+
+def find_median(numbers):
+    numbers.sort()
+    if len(numbers) % 2 == 1:
+        return numbers[len(numbers) // 2]
+    else:
+        return (numbers[len(numbers) // 2 - 1] + numbers[len(numbers) // 2]) / 2
+
+# print(find_median([13, 7, -3, 82, 4]))
+# print(find_median([7, 82, 4, -7, 13]))
+# print(find_median([64, 75, 2, -13, 189]))
